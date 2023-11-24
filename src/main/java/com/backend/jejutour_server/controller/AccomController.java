8370ef72275e906976aa29ac7f16382e313863f2
Hyeonseok0627 <stonehead0627@gmail.com>
2023-11-24 11:30:54 +0900

    AccomController.java 내 fesList -> accomList로 수정(231124)

--- src/main/java/com/backend/jejutour_server/controller/AccomController.java
+++ src/main/java/com/backend/jejutour_server/controller/AccomController.java
@@ -23,7 +23,7 @@ public class AccomController {
 
     // 전체 출력
     @GetMapping("/accomAllList")
-    public List<AccomEntity> FesList() {
+    public List<AccomEntity> AccomList() {
         List<AccomEntity> accomList = accomService.getAllAccomList();
         return accomList;
     }
@@ -33,26 +33,60 @@ public class AccomController {
     @GetMapping("/accomList/{itemsRegion2CdValue}")
     public List<AccomEntity> AccomItemsRegion2CdValueList(@PathVariable("itemsRegion2CdValue") Long itemsRegion2CdValue) throws UnsupportedEncodingException {
 
-//            case 11 : test = "제주시내"; break;
-//            case 12 : test = "애월"; break;
-//            case 13 : test = "한림"; break;
-//            case 14 : test = "한경"; break;
-//            case 15 : test = "조천"; break;
-//            case 16 : test = "구좌"; break;
-//            case 17 : test = "성산"; break;
-//            case 21 : test = "서귀포시내"; break;
-//            case 22 : test = "대정"; break;
-//            case 23 : test = "안덕"; break;
-//            case 24 : test = "중문"; break;
-//            case 25 : test = "남원"; break;
-//            case 26 : test = "표선"; break;
-//            case 31 : test = "우도"; break;
-//            case 32 : test = "추자도"; break;
-//            case 33 : test = "마라도"; break;
+        String regionName;
 
-        List<AccomEntity> accomList = accomService.getitemsRegion2CdValueAccomList(itemsRegion2CdValue);
-        return accomList;
+        switch (itemsRegion2CdValue.intValue()) {
+            case 11:
+                regionName = "제주시내";
+                break;
+            case 12:
+                regionName = "애월";
+                break;
+            case 13:
+                regionName = "한림";
+                break;
+            case 14:
+                regionName = "한경";
+                break;
+            case 15:
+                regionName = "조천";
+                break;
+            case 16:
+                regionName = "구좌";
+                break;
+            case 17:
+                regionName = "성산";
+                break;
+            case 21:
+                regionName = "서귀포시내";
+                break;
+            case 22:
+                regionName = "대정";
+                break;
+            case 23:
+                regionName = "안덕";
+                break;
+            case 24:
+                regionName = "중문";
+                break;
+            case 25:
+                regionName = "남원";
+                break;
+            case 26:
+                regionName = "표선";
+                break;
+            case 31:
+                regionName = "우도";
+                break;
+            case 32:
+                regionName = "추자도";
+                break;
+            case 33:
+                regionName = "마라도";
+                break;
+        }
+            List<AccomEntity> accomList = accomService.getitemsRegion2CdValueAccomList(itemsRegion2CdValue);
+
+            return accomList;
+        }
     }
-
-
-}
